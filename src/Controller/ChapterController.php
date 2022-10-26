0d8d46f78901b2128147fc9d501ff9d4fe794f5d
Toussaint Charlie <charlie.toussaint@hotmail.fr>
2022-10-26 15:41:30 +0200

    The view of the chapters is now linked to the table and chapters are generated automaticly with buttons pre and next

---
+++ src/Controller/ChapterController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\ChapterManager;
+
+class ChapterController extends AbstractController
+{
+    /**
+     * Show informations for a specific chapter
+     */
+    public function show(int $id): string
+    {
+        $chapterManager = new ChapterManager();
+        $chapter = $chapterManager->selectOneById($id);
+
+        return $this->twig->render('Chapter/show.html.twig', ['chapter' => $chapter]);
+    }
+}
