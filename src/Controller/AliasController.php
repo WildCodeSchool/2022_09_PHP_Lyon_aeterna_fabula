24c4e8f3db92a9735a9b865572b1a62342aaeda0
79Inaf <fanny.sauvageot@gmail.com>
2022-11-16 11:04:43 +0100

    function create in AliasController and function addAlias in AliasController

--- src/Controller/AliasController.php
+++ src/Controller/AliasController.php
@@ -7,13 +7,59 @@ use App\Model\HistoricManager;
 
 class AliasController extends AbstractController
 {
+    private array $errors = [];
+    /**
+     * String Check
+     */
+    public function stringCheck(array $alias, string $field, string $fieldFrench, int $maxLength): void
+    {
+        if (!isset($alias[$field]) || empty($alias[$field])) {
+            $this->errors[] = "$fieldFrench est obligatoire";
+        }
+        if (strlen($alias[$field]) > $maxLength) {
+            $this->errors[] = "$fieldFrench ne doit pas dépasser $maxLength caractères";
+        }
+    }
+
+    /**
+     * Form Control
+     */
+    public function formControl(array $alias): void
+    {
+        // TODO validations (length, format...)
+        $this->stringCheck($alias, 'player_name', 'L\'alias', 55);
+    }
     public function start()
     {
         return $this->twig->render('Alias/alias_index.html.twig');
     }
 
-    public function create()
+    public function create(int $user_id = 1): ?string
     {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // clean $_POST data
+            $alias = array_map('trim', $_POST);
+            $this->formControl($alias);
+
+
+            // TODO validations (length, format...)
+
+            if (!empty($this->errors)) {
+                return $this->twig->render('Alias/alias_create.html.twig', [
+                    'errors' => $this->errors,
+                ]);
+            }
+
+            // if validation is ok, insert and redirection
+
+            // récuperer l'id user de la session : voir avec les experts
+            $aliasManager = new AliasManager();
+            $aliasManager->addAlias($alias, $user_id);
+
+            header('Location:/incipit');
+            return null;
+        }
+
         return $this->twig->render('Alias/alias_create.html.twig');
     }
 }
--- src/Model/AliasManager.php
+++ src/Model/AliasManager.php
@@ -7,4 +7,29 @@ use PDO;
 class AliasManager extends AbstractManager
 {
     public const TABLE = 'alias';
+
+    //insert new alias
+
+    public function addAlias(array $alias, int $user_id): void
+    {
+        $query = "INSERT INTO " . self::TABLE . " (
+            `player_name`,
+            `nature`,
+            `user_id`
+            )
+
+             VALUES (
+                :player_name,
+                'ONGOING',
+                :user_id          
+                )";
+
+        $statement = $this->pdo->prepare($query);
+
+        $statement->bindValue('player_name', $alias['player_name'], PDO::PARAM_STR);
+        $statement->bindValue('user_id', $user_id, PDO::PARAM_INT);
+
+
+        $statement->execute();
+    }
 }
