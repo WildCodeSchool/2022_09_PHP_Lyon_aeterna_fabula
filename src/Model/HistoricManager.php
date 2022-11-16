e721895138f06ab967b8beac894402093cf035f2
emainguelag <110185005+emainguelag@users.noreply.github.com>
2022-11-16 22:34:18 +0100

    add detail accordion in historic

--- src/Model/HistoricManager.php
+++ src/Model/HistoricManager.php
@@ -30,9 +30,10 @@ class HistoricManager extends AbstractManager
     public function selectActionsByHistoric(): array|false
     {
         // prepared request
-        $query = "SELECT h.id, h.action_id, h.historic_date, a.id AS actionID, a.label
-            FROM historic AS h
-            LEFT JOIN action AS a ON h.action_id  = a.id;";
+        $query = "SELECT h.id, h.action_id, h.historic_date, a.id AS actionID, a.label, c.number, c.title, c.description
+        FROM historic AS h
+        LEFT JOIN action AS a ON h.action_id  = a.id
+        LEFT JOIN chapter AS c ON a.owner_id = c.id;";
 
         return $this->pdo->query($query)->fetchAll();
     }
